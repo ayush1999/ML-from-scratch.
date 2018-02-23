a53fe92188b6bc8504f85bddb12aaf5af8d45beb
ayush1999 <ayush.shridhar1999@gmail.com>
2018-02-23 23:02:56 +0530

    LinReg

--- src/linear.py
+++ src/linear.py
@@ -31,19 +31,21 @@ def transpose(X):
 def inverse(X):
     return np.linalg.inv(X)
 
-def theta_final(X, Y):
-    return (inverse(transpose(X) * X) * transpose(X)) * Y
+def train(X, Y):
+    temp = np.dot(transpose(X), X)
+    temp = inverse(temp)
+    temp = np.dot(temp, transpose(X))
+    return np.dot(temp, Y)
 
 def multiply(a, b):
     return np.dot(a, b)
 
 if __name__=="__main__":
     filename = input("Enter the name of the file. ")
-    X = read("dataset.csv")
+    X = read("train.csv")
     Y= []
     for ele in X:
         Y.append(ele[-1])
-    print(Y)
     X = np.ndarray.tolist(X)
     for ele in X:
         ele.remove(ele[-1])
@@ -53,5 +55,29 @@ if __name__=="__main__":
         res.append(ele)
     X = res
     X = np.array(X)
-    print("Desired values of theta vector are:{}".format(theta_final(X, Y)))
-    
+    res = train(X, Y)
+    res1 = np.array(res)
+    print("The estimated values are: {}".format(res))
+
+    # test
+    X = read("test.csv")
+    Y = []
+    for ele in X:
+        Y.append(ele[-1])
+    X = np.ndarray.tolist(X)
+    for ele in X:
+        ele.remove(ele[-1])
+    res = []
+    for ele in X:
+        ele = [1] + ele
+        res.append(ele)
+    X = res
+    X = np.array(X)
+
+    loss=0
+
+    for i in range(len(X)):
+        loss = loss + (np.dot(transpose(res1), X[i]) - Y[i])**2
+
+    print("Net loss is: {}".format(loss))
+
